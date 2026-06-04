2b01c5bc8529e8e10953a3a4fdd2de198b30566f
Jean <24101123@ue.edu.pe>
2026-06-04 04:04:44 -0500

    US-007 Confirmación de pago de transacción

--- KAMBIO.API/Program.cs
+++ KAMBIO.API/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDbContext<KambioDbContext>(options =>
 
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IConfirmacionPagoRepository, ConfirmacionPagoRepository>();
+builder.Services.AddScoped<IConfirmacionPagoService, ConfirmacionPagoService>();
 
 // Add services to the container.
 
