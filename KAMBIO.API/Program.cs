b5dd465bd555d01b0668d6030d048b40d61dffa7
24101274-oss <24101274@ue.edu.pe>
2026-06-04 03:19:50 -0500

    US-020 . Bloqueo y suspensión de cuentas de usuario

--- KAMBIO.API/Program.cs
+++ KAMBIO.API/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddDbContext<KambioDbContext>(options =>
 
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
-
+builder.Services.AddScoped<IAdministracionUsuarioRepository, AdministracionUsuarioRepository>();
+builder.Services.AddScoped<IAdministracionUsuarioService, AdministracionUsuarioService>();
 // Add services to the container.
 
 builder.Services.AddControllers();
